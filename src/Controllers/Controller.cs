049e4cc992c161ee66e8c0a09ef16cd4a55ce02d
Tailwind Mechanics <shea@tailwindmechanics.com>
2023-11-22 20:24:35 +0000

    Standardised ship logging for endpoints

--- src/Controllers/Controller.cs
+++ src/Controllers/Controller.cs
@@ -1,11 +1,12 @@
 //path: src\Controllers\Controller.cs
 
 using Microsoft.AspNetCore.Mvc;
+using Confluent.Kafka;
 
 using Neurocache.OperationChannels;
+using Neurocache.ShipsInfo;
 using Neurocache.Utilities;
 using Neurocache.Schema;
-using Confluent.Kafka;
 
 namespace Neurocache.Controllers.Agent
 {
@@ -19,6 +20,8 @@ namespace Neurocache.Controllers.Agent
         {
             if (!Keys.Guard(Request)) return Unauthorized();
 
+            Ships.Log("Killing all operations");
+
             OperationChannelService.Instance.KillAll();
             return Ok();
         }
@@ -27,6 +30,8 @@ namespace Neurocache.Controllers.Agent
         public IActionResult Stop([FromBody] StopAgentRequest body)
         {
             if (!Keys.Guard(Request)) return Unauthorized();
+
+            Ships.Log("Stopping operation");
 
             var tokenGuid = Guid.Parse(body.SessionToken);
             OperationChannelService.Instance.StopOperationChannel(tokenGuid);
@@ -37,6 +42,8 @@ namespace Neurocache.Controllers.Agent
         public async Task<IActionResult> Run([FromBody] OperationRequestData operationRequest)
         {
             if (!Keys.Guard(Request, out var apiKey)) return Unauthorized();
+
+            Ships.Log("Recieved operation request");
 
             var operationToken = await Vanguard.Notice.OperationRequest(apiKey, operationRequest);
             if (operationToken is null) return Unauthorized();
