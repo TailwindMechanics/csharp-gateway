00787dbe30b2b7dbc4a15d53a6feaa9ab1d2651b
Tailwind Mechanics <shea@tailwindmechanics.com>
2023-11-27 18:36:41 +0000

    Added graceful disconnection

--- src/ClientChannels/ClientChannel.cs
+++ src/ClientChannels/ClientChannel.cs
@@ -15,20 +15,39 @@ namespace Neurocache.ClientChannels
         public async Task StartCommunication()
         {
             var buffer = new byte[1024 * 4];
-            WebSocketReceiveResult result;
-
-            do
+            try
             {
-                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                if (result.MessageType == WebSocketMessageType.Text && !result.CloseStatus.HasValue)
-                {
-                    var receivedMessage = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                    OnMessageReceived(receivedMessage);
-                }
-            }
-            while (!result.CloseStatus.HasValue);
+                WebSocketReceiveResult result;
 
-            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+                do
+                {
+                    result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                    if (result.MessageType == WebSocketMessageType.Text && !result.CloseStatus.HasValue)
+                    {
+                        var receivedMessage = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                        Ships.Log($"Message received on operation token {operationToken}: {receivedMessage}");
+                        OnMessageReceived(receivedMessage);
+                        // Echo
+                        await SendMessageAsync($"Vanguard received: {receivedMessage}");
+                    }
+                    else if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        Ships.Log($"WebSocket closing received for operation token {operationToken}");
+                    }
+                }
+                while (!result.CloseStatus.HasValue);
+
+                Ships.Log($"Starting close handshake for operation token {operationToken}");
+                await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+            }
+            catch (WebSocketException ex) when (ex.WebSocketErrorCode == WebSocketError.ConnectionClosedPrematurely)
+            {
+                Ships.Log($"WebSocket connection closed prematurely for operation token {operationToken}: {ex.Message}");
+            }
+            finally
+            {
+                Ships.Log($"Cleaning up WebSocket for operation token {operationToken}");
+            }
 
             Ships.Log("WebSocket connection closed for operation token: " + operationToken);
         }
