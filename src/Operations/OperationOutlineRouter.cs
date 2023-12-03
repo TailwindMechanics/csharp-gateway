003c0080636e010143aa9be05c5c391c17a64f84
Tailwind Mechanics <shea@tailwindmechanics.com>
2023-12-03 19:59:13 +0000

    Adding more logging to routing

--- src/Operations/OperationOutlineRouter.cs
+++ src/Operations/OperationOutlineRouter.cs
@@ -11,39 +11,58 @@ namespace Neurocache.Operations
         {
             var result = new List<OperationReport>();
 
+            // Find the Current Node
             var currentNode = outline.Nodes.FirstOrDefault(n => n.Id == previousReport.ReportId.ToString());
             if (currentNode == null)
             {
-                Ships.Warning($"NextReport: No node found for report ID {previousReport.ReportId}");
+                Ships.Log($"NextReport: Node with ID {previousReport.ReportId} not found.");
                 return result;
             }
 
-            Ships.Log($"NextReport: Found node for report ID {previousReport.ReportId}");
+            Ships.Log($"NextReport: Found current node with ID {currentNode.Id}.");
 
-            foreach (var handle in currentNode.Data.Handles.Where(h => h.HandleType == "source"))
+            // Check the Node's Output Handles
+            var sourceHandles = currentNode.Data.Handles.Where(h => h.HandleType == "source").ToList();
+            if (!sourceHandles.Any())
             {
-                var nextNodeIds = outline.Edges
+                Ships.Log($"NextReport: No output (source) handles found for node {currentNode.Id}.");
+            }
+            else
+            {
+                Ships.Log($"NextReport: Found {sourceHandles.Count} output (source) handle(s) for node {currentNode.Id}.");
+            }
+
+            // Find Outgoing Connections
+            foreach (var handle in sourceHandles)
+            {
+                Ships.Log($"NextReport: Checking connections from handle {handle.Id} of node {currentNode.Id}.");
+
+                var targetNodeIds = outline.Edges
                     .Where(e => e.Source == currentNode.Id && e.SourceHandle == handle.Id)
                     .Select(e => e.Target)
                     .ToList();
 
-                if (!nextNodeIds.Any())
+                if (!targetNodeIds.Any())
                 {
-                    Ships.Log($"NextReport: No next nodes found for source handle ID {handle.Id}");
-                    continue;
+                    Ships.Log($"NextReport: No target nodes found for source handle ID {handle.Id} of node {currentNode.Id}.");
+                }
+                else
+                {
+                    Ships.Log($"NextReport: Found {targetNodeIds.Count} target node(s) for source handle ID {handle.Id} of node {currentNode.Id}.");
                 }
 
-                foreach (var targetNodeId in nextNodeIds)
+                // Identify Next Nodes
+                foreach (var targetNodeId in targetNodeIds)
                 {
                     var targetNode = outline.Nodes.FirstOrDefault(n => n.Id == targetNodeId);
                     if (targetNode == null)
                     {
-                        Ships.Warning($"NextReport: No node found for target node ID {targetNodeId}");
+                        Ships.Log($"NextReport: Target node with ID {targetNodeId} not found.");
                         continue;
                     }
 
-                    Ships.Log($"NextReport: Creating report for target node ID {targetNodeId}");
-                    result.Add(new OperationReport(
+                    // Create Reports for Next Nodes
+                    var newReport = new OperationReport(
                         previousReport.Token,
                         Ships.ThisVessel,
                         targetNode.NodeType!,
@@ -51,8 +70,21 @@ namespace Neurocache.Operations
                         previousReport.AgentId,
                         false,
                         targetNode.Id!
-                    ));
+                    );
+
+                    result.Add(newReport);
+                    Ships.Log($"NextReport: Created new report for target node ID {targetNode.Id}.");
                 }
+            }
+
+            // Return the Reports
+            if (result.Any())
+            {
+                Ships.Log($"NextReport: Generated {result.Count} new report(s) for processing.");
+            }
+            else
+            {
+                Ships.Log("NextReport: No new reports generated for further processing.");
             }
 
             return result;
