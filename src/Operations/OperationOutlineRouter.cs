8aa7b4d8238d6670e101938b2eca3a949a2253ff
Tailwind Mechanics <shea@tailwindmechanics.com>
2023-12-03 19:24:35 +0000

    Updated report routing and OperationReport ToString method

--- src/Operations/OperationOutlineRouter.cs
+++ src/Operations/OperationOutlineRouter.cs
@@ -9,47 +9,54 @@ namespace Neurocache.Operations
     {
         public static List<OperationReport> NextReport(OperationOutline outline, OperationReport previousReport)
         {
-            var dependents = new List<Node>();
-            var prevNode = outline.Nodes
-                .FirstOrDefault(n => n.Id == previousReport.ReportId.ToString());
-
-            if (prevNode == null)
-            {
-                Ships.Warning($"OperationOutlineRouter: Node with id {previousReport.ReportId} not found");
-                return [];
-            }
-
-            foreach (var handle in prevNode.Data.Handles)
-            {
-                var nextNodeIds = outline.Edges
-                  .Where(e => e.Source == handle.Id)
-                  .Select(e => e.Target)
-                  .ToList();
-
-                var nextNodes = outline.Nodes.Where(n => nextNodeIds.Contains(n.Id));
-
-                foreach (var nextNode in nextNodes)
-                {
-                    if (nextNode == null) continue;
-
-                    dependents.Add(nextNode);
-                }
-            }
-
             var result = new List<OperationReport>();
-            foreach (var node in dependents)
-            {
-                if (node == null || node.NodeType == null || node.Id == null) continue;
 
-                result.Add(new OperationReport(
-                    previousReport.Token,
-                    Ships.ThisVessel,
-                    node.NodeType,
-                    previousReport.Payload,
-                    previousReport.AgentId,
-                    false,
-                    node.Id
-                ));
+            // Find the node corresponding to the previous report's ID.
+            var currentNode = outline.Nodes.FirstOrDefault(n => n.Id == previousReport.ReportId.ToString());
+            if (currentNode == null)
+            {
+                Ships.Warning($"NextReport: No node found for report ID {previousReport.ReportId}");
+                return result;
+            }
+
+            Ships.Log($"NextReport: Found node for report ID {previousReport.ReportId}");
+
+            // Process each handle of the current node.
+            foreach (var handle in currentNode.Data.Handles)
+            {
+                // Find all target node IDs connected to the current handle.
+                var targetNodeIds = outline.Edges
+                    .Where(e => e.Source == handle.Id)
+                    .Select(e => e.Target)
+                    .ToList();
+
+                if (!targetNodeIds.Any())
+                {
+                    Ships.Log($"NextReport: No target nodes found for handle ID {handle.Id}");
+                    continue;
+                }
+
+                // Retrieve the actual node data for each target node ID.
+                foreach (var targetNodeId in targetNodeIds)
+                {
+                    var targetNode = outline.Nodes.FirstOrDefault(n => n.Id == targetNodeId);
+                    if (targetNode == null)
+                    {
+                        Ships.Warning($"NextReport: No node found for target node ID {targetNodeId}");
+                        continue;
+                    }
+
+                    Ships.Log($"NextReport: Creating report for target node ID {targetNodeId}");
+                    result.Add(new OperationReport(
+                        previousReport.Token,
+                        Ships.ThisVessel,
+                        targetNode.NodeType!,
+                        previousReport.Payload,
+                        previousReport.AgentId,
+                        false,
+                        targetNode.Id!
+                    ));
+                }
             }
 
             return result;
